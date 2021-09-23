ae95e178d9b7c6e1a930fa6be3d94b4cf4a63884
Savelii <s.i.khizhnikov@student.khai.edu>
2021-09-24 00:27:47 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,3 +1,8 @@
 print("""Hello! My name is Hans.
-I was created in 2021.""")
+I was created in 2021.
+Please, remind me your name.""")
 
+name = str(input())
+
+print("""What a great name you have, """ + name + """!
+Enter remainders of diving your age by 3, 5, 7.""")
