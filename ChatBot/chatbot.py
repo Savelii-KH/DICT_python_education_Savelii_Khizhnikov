02ff52e9a78af22569ffb5a2073967e1d0d872ea
Savelii <s.i.khizhnikov@student.khai.edu>
2021-09-24 00:28:38 +0300

    ChatBot 3-rd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -6,3 +6,13 @@ name = str(input())
 
 print("""What a great name you have, """ + name + """!
 Enter remainders of diving your age by 3, 5, 7.""")
+
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 + 15) % 105
+age = str(age)
+
+print("""Your age is """ + age + """, that's good time to start programming!")
+Now I will prove to you that I can count to any number you want""")
