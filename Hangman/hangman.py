e8572ff8c599a1119aadc3114bd101a46af3fb8e
Savelii <s.i.khizhnikov@student.khai.edu>
2021-10-10 21:37:09 +0300

    Hangman 5-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,15 +1,31 @@
 import random
 
-words = ("python", "pascal", "java", "javascript")
-helped_word_list = ("pyt---", "pas---", "jav-", "jav-------")
+code_words = ["python", "java", "javascript", "php"]
 word_number = random.randint(0, 3)
-word = words[word_number]
-helped_word = helped_word_list[word_number]
-print("HANGMAN")
+word = list(code_words[word_number])
+length = len(word)
+replaced_word = ["-" for letter in word]
+print("""       HANGMAN         
+If you want play print "Start", else you exit from game
+""")
+joined_word = ("".join(word))
+x = 8
 
-input_word = input("Guess the word " + helped_word + ": ")
+start = input()
+if start == "Start":
+    while x > 0:
+        print("".join(replaced_word))
+        print("Your tries: " + str(x))
+        letter = str(input("Input a letter: ").strip(""))
+        if letter in word:
+            for i, c in enumerate(word):
+                if letter == c:
+                    replaced_word[i] = letter
+            if "-" not in replaced_word:
+                print("Your word is: '" + str("".join(replaced_word)) + "', and you won!")
+                break
+        else:
+            x -= 1
+    if x == 0:
+        print("You lost")
 
-if input_word == word:
-    print("You win!")
-else:
-    print("You lost!")
