a6bc1493c182ae1d79ec6f0140e23797bc95ee68
Savelii <s.i.khizhnikov@student.khai.edu>
2021-10-10 21:35:58 +0300

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,11 +1,13 @@
 import random
 
-words = ("python", "php", "java", "javascript")
-word = random.choice(words)
-
+words = ("python", "pascal", "java", "javascript")
+helped_word_list = ("pyt---", "pas---", "jav-", "jav-------")
+word_number = random.randint(0, 3)
+word = words[word_number]
+helped_word = helped_word_list[word_number]
 print("HANGMAN")
 
-input_word = input("Guess the word: ")
+input_word = input("Guess the word " + helped_word + ": ")
 
 if input_word == word:
     print("You win!")
