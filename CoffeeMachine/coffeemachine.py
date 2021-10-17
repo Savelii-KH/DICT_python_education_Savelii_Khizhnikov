55230b6f3eab4a7d71c726edca8c49d3e1d7915b
Savelii <s.i.khizhnikov@student.khai.edu>
2021-10-17 13:28:45 +0300

    CoffeeMachine 3 stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -1,9 +1,18 @@
-print("Write how many cups of coffee you will need:")
-cups = input()
-watter = int(cups) * 200
-milk = int(cups) * 50
-beans = int(cups) * 15
-print("For " + cups + " cups of coffee you will need:")
-print(str(watter) + " ml of watter")
-print(str(milk) + " ml of milk")
-print(str(beans) + " g of coffee beans")
+water_in = int(input("Write how many ml of water the coffee machine has: "))
+milk_in = int(input("Write how many ml of milk the coffee machine has: "))
+beans_in = int(input("Write how may g of coffee beans the coffee machine has: "))
+needed_cups = int(input("Write how many cups of coffee you will need: "))
+
+water = water_in // 200
+milk = milk_in // 50
+beans = beans_in // 15
+
+ing = (water, milk, beans)
+min_ing = min(ing)
+
+if needed_cups == min_ing:
+    print("Yes, I can make that amount of coffee")
+elif needed_cups > min_ing:
+    print("No, I can make only " + str(min_ing) + " cups of coffee")
+elif needed_cups < min_ing:
+    print("Yes, I can make that amount of coffee (and even " + str(int(min_ing-needed_cups)) + " more than that)")
