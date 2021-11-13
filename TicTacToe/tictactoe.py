409d2cd9bbdd98bac26356fab15f7a204d747720
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2021-11-13 21:48:18 +0200

    TicTacToe 2 stage

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,4 +1,25 @@
-print("""X O X
-O X O
-X O X
-""")
+xo_list = input("Enter cells: ")
+ceil1 = xo_list[0:3]
+ceil2 = xo_list[3:6]
+ceil3 = xo_list[6:9]
+o_list = ceil1.count("O") + ceil2.count("O") + ceil3.count("O")
+x_list = ceil1.count("X") + ceil2.count("X") + ceil3.count("X")
+
+
+def pole():
+    global ceil1, ceil2, ceil3
+    print("|", end=" ")
+    for a in ceil1:
+        print(a, end=" ")
+    print("|")
+    print("|", end=" ")
+    for b in ceil2:
+        print(b, end=" ")
+    print("|")
+    print("|", end=" ")
+    for c in ceil3:
+        print(c, end=" ")
+    print("|")
+
+
+pole()
