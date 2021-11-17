33dd12084c346381324c64f557e0bf2639bbb1be
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2021-11-17 23:30:41 +0200

    CreditCalculator 2.1 stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,3 +1,5 @@
+import math
+
 print("""Loan principal: 1000
 Month 1: repaid 250
 Month 2: repaid 250
@@ -10,10 +12,13 @@ type \"m\" - for number of  monthly payments
 type \"p\" - for the monthly payment""")
 choice = str(input())
 if choice == "m":
-    month = int(input("Enter the monthly payment: "))
-    print(f"It will take {round(principal/month)} to repay the loan ")
+    payments = int(input("Enter the monthly payment: "))
+    print(f"It will take {round(principal/payments)} to repay the loan ")
 if choice == "p":
-    payment = int(input("Enter the number of months: "))
-    print(f"Your month payment = {round(principal/payment)}")
-pp = float(input())
-print(round(pp))
+    month = int(input("Enter the number of months: "))
+    payment = math.ceil(principal/month)
+    lat_payment = int(principal - (month - 1) * payment)
+    if lat_payment == payment:
+        print(f"Your month payment = {payment}")
+    else:
+        print(f"Your month payment = {payment}, and the last payment = {lat_payment}")
