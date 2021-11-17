d6f573079a082d11ab4a176d9b2fbfc360479ddf
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2021-11-18 00:40:19 +0200

    CreditCalculator 3 stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,24 +1,34 @@
 import math
 
-print("""Loan principal: 1000
-Month 1: repaid 250
-Month 2: repaid 250
-Month 3: repaid 500
-The loan has been repaid!
-""")
-principal = int(input("Enter the loan principal: "))
 print("""What do you want calculate?
-type \"m\" - for number of  monthly payments
-type \"p\" - for the monthly payment""")
+type \"n\" for number of monthly payments,
+type \"a\" for annuity monthly payment amount,
+type \"p\" for loan principal:""")
 choice = str(input())
-if choice == "m":
-    payments = int(input("Enter the monthly payment: "))
-    print(f"It will take {round(principal/payments)} to repay the loan ")
+if choice == "n":
+    principal = int(input("Enter the loan principal: "))
+    payment = int(input("Enter the monthly payment: "))
+    interest = int(input("Enter the loan interest: "))
+    percent = float(interest/(12 * 100))
+    month = math.ceil(math.log(payment/(payment - percent * principal), 1 + percent))
+    years = round(month/12)
+    months = month - years * 12
+    print(f"It will take {years} years and {months} months to repay this loan!")
+    if years == 0:
+        print(f"It will take {months} months to repay this loan!")
+    if months == 0:
+        print(f"It will take {years} years to repay this loan!")
+if choice == "a":
+    principal = int(input("Enter the loan principal: "))
+    month = int(input("Enter the number of periods: "))
+    interest = int(input("Enter the loan interest: "))
+    percent = float(interest/(12 * 100))
+    payment = math.ceil(principal * ((percent * ((1 + percent) ** month)) / (((1 + percent) ** month) - 1)))
+    print(f"Your monthly payment = {payment}!")
 if choice == "p":
-    month = int(input("Enter the number of months: "))
-    payment = math.ceil(principal/month)
-    lat_payment = int(principal - (month - 1) * payment)
-    if lat_payment == payment:
-        print(f"Your month payment = {payment}")
-    else:
-        print(f"Your month payment = {payment}, and the last payment = {lat_payment}")
+    payment = float(input("Enter the monthly payment: "))
+    month = float(input("Enter the number of periods: "))
+    interest = float(input("Enter the loan interest: "))
+    percent = float(interest/(12 * 100))
+    principal = round(payment / ((percent * ((1 + percent) ** month)) / (((1 + percent) ** month) - 1)))
+    print(f"Your loan principal = {principal}!")
