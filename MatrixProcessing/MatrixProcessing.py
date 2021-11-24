da71eb4937d284d0507536553eea02ae63543470
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2021-11-24 17:12:47 +0200

    MatrixProcessing 2 stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -7,19 +7,9 @@ for i in range(int(mat_size_a[0])):
         print("Incorrect size")
         break
 
-mat_size_b = input().split()
-mat_b = []
-for c in range(int(mat_size_b[0])):
-    line = input().split()
-    mat_b.append(line)
-    if len(mat_b[c]) != int(mat_size_b[1]):
-        print("Incorrect size")
-        break
+const = input()
 
-if mat_size_a == mat_size_b:
-    for i in range(len(mat_a)):
-        for j in range(len(mat_a[i])):
-            print(int(mat_a[i][j]) + int(mat_b[i][j]), end=" ")
-        print(" ")
-if mat_size_a != mat_size_b:
-    print("ERROR")
+for i in range(len(mat_a)):
+    for j in range(len(mat_a[i])):
+        print(int(mat_a[i][j]) * int(const), end=" ")
+    print(" ")
