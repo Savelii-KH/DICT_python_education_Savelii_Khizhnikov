d52d45e2f5114b9abed46d2fb4caae5192252b8d
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2021-12-01 21:11:35 +0200

    DinnerParty 1.1 stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,12 +1,9 @@
 print("Enter the number of friends joining (including you): ")
 count = int(input("• "))
-names = {}
 
 if count > 0:
     print("Enter names: ")
-    for i in range(0, count):
-        name = input("• ")
-        names[name] = 0
+    names = {input("• "): 0 for i in range(count)}
     print(names)
 else:
     print("No one is joining for the party")
