c9319a16d3ceabfa79386d428809ae54b9e00e3a
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2021-12-07 19:48:18 +0200

    Rock-Paper-Scissors 1 stage

---
+++ Rock_Paper_Scissors/rock_paper_scissors.py
@@ -0,0 +1,8 @@
+print("Print rock, scissors or paper")
+a = input("· ")
+if a == "rock":
+    print("Sorry, but the computer chose paper")
+if a == "scissors":
+    print("Sorry, but the computer chose rock")
+if a == "paper":
+    print("Sorry, but the computer chose scissors")
