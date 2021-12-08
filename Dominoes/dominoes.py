cb8bdd97bd49c681fb7513bb956855a5f6c2114e
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2021-12-08 20:20:50 +0200

    Dominoes 5 stage

--- Dominoes/dominoes.py
+++ Dominoes/dominoes.py
@@ -1,4 +1,5 @@
 import random
+from itertools import chain
 dominoes = [[x, y] for x in range(0, 7) for y in range(x, 7)]
 random.shuffle(dominoes)
 player_d = []
@@ -35,9 +36,9 @@ else:
 def pole():
     print("=" * 70)
     print(f"Stock size: {len(dominoes)}")
-    print(f"Computer pieces: {len(comp_d)} \n")
+    print(f"Computer pieces: {len(logic)} \n")
     if len(pole_snake) <= 6:
-        print(pole_snake)
+        print(*pole_snake)
     else:
         print(f"{pole_snake[:3]} ... {pole_snake[-3:]}")
     print("\nYour pieces:")
@@ -75,50 +76,57 @@ def player_move(player_action):
         print("Invalid input. Please try again.")
 
 
-def comp_move():
-    global move
-    while True:
-        comp_action = random.randint(-len(comp_d)+1, len(comp_d)-1)
-        if 0 < comp_action <= len(comp_d):
-            if pole_snake[-1][1] == comp_d[comp_action][0]:
-                pole_snake.append(comp_d[comp_action])
-                comp_d.remove(comp_d[comp_action])
-                move = "player"
-                break
-            elif pole_snake[-1][1] == comp_d[comp_action][1]:
-                pole_snake.append(comp_d[comp_action][::-1])
-                comp_d.remove(comp_d[comp_action])
-                move = "player"
-                break
-            else:
-                continue
-        elif -len(comp_d) <= comp_action < 0:
-            if pole_snake[0][0] == comp_d[-comp_action - 1][1]:
-                pole_snake.insert(0, comp_d[-comp_action-1])
-                comp_d.remove(comp_d[comp_action])
-                move = "player"
-                break
-            elif pole_snake[0][0] == comp_d[-comp_action - 1][0]:
-                pole_snake.insert(0, comp_d[-comp_action - 1][::-1])
-                comp_d.remove(comp_d[comp_action])
-                move = "player"
-                break
-            else:
-                continue
-        elif comp_action == 0:
-            comp_d.append(dominoes[0])
-            dominoes.remove(dominoes[0])
-            move = "player"
-            break
+def comp_logic(comp_d):
+    len_comp_d = len(comp_d)
+    comp_d = list(chain(*comp_d))
+    value = {}
+    for i in range(0, 7):
+        value[i] = comp_d.count(i)
+    last_dict = {}
+    len_comp_dm = len(comp_d)
+    for i in range(len_comp_d):
+        comp_d.append(comp_d[i+i:2*i+2])
+    del comp_d[:len_comp_dm]
+    for i in comp_d:
+        value_index = value.get(i[0])
+        value_index += value.get(i[1])
+        last_dict[comp_d.index(i)] = value_index
+    max_value = sorted(last_dict.items(), key=lambda x: x[1], reverse=True)
+    last_dict = [comp_d[c[0]] for c in max_value]
+    return last_dict
 
+
+logic = comp_logic(comp_d)
 
 pole()
 while True:
     if move == "computer":
         print("Status: Computer is about to make a move. Press Enter to continue...")
         input()
-        move = "player"
-        comp_move()
+        for i in range(len(logic)):
+            if logic[i][0] == pole_snake[-1][1]:
+                pole_snake.append(logic[i])
+                logic.remove(logic[i])
+                move = "player"
+                break
+            elif logic[i][1] == pole_snake[-1][1]:
+                pole_snake.append(logic[i][::-1])
+                logic.remove(logic[i])
+                move = "player"
+                break
+            elif logic[i][1] == pole_snake[0][0]:
+                pole_snake.insert(0, logic[i])
+                logic.remove(logic[i])
+                move = "player"
+                break
+            elif logic[i][0] == pole_snake[0][0]:
+                pole_snake.insert(0, logic[i][::-1])
+                logic.remove(logic[i])
+                move = "player"
+                break
+        else:
+            logic.append(dominoes[0])
+            move = "player"
     else:
         player_choice = input("Status: It's your turn to make a move. Enter your command: ")
         try:
