5c8458b44f654f404e31d8985050c7895ef7afb7
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-02-07 16:04:24 +0200

    Arithmatic-Test stage #2

--- Arithmetic_Test/arithmetic-test.py
+++ Arithmetic_Test/arithmetic-test.py
@@ -1,19 +1,32 @@
 import random
 
 sl = ["+", "-", "*"]
-a = random.randint(2, 9)
-b = random.randint(2, 9)
-s = random.randint(0, 2)
+x = 0
+n = 0
 answer = None
-if s == 0:
-    answer = a + b
-elif s == 1:
-    answer = a - b
-else:
-    answer = a * b
-print(f"{a} {sl[s]} {b}")
-action = int(input("> "))
-if action == answer:
-    print("Right!")
-else:
-    print("Wrong!")
+
+while x < 5:
+    a = random.randint(2, 9)
+    b = random.randint(2, 9)
+    s = random.randint(0, 2)
+    print(f"{a} {sl[s]} {b}")
+    if s == 0:
+        answer = a + b
+    elif s == 1:
+        answer = a - b      
+    else:
+        answer = a * b
+    while True:
+        try:
+            action = int(input("> "))
+            if action == answer:
+                print("Right")
+                n += 1
+                break
+            else:
+                print("Wrong")
+                break
+        except ValueError:
+            print("Incorrect format")
+    x += 1
+print(f"You mark is {n}/5.")
