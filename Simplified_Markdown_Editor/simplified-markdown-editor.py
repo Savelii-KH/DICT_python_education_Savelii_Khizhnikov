623ad0c7428d1513390adfc4c00ef6278392638d
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-02-15 16:53:24 +0200

    Simplified_Markdown_Editor stage #2

--- Simplified_Markdown_Editor/simplified-markdown-editor.py
+++ Simplified_Markdown_Editor/simplified-markdown-editor.py
@@ -1,12 +1,34 @@
+markdown = []
+str_parameter = "plain bold italic header link inline-code ordered-list unordered-list new-line"
+parameter = ["plain", "bold", "italic",
+             "header", "link", "inline-code",
+             "ordered-list", "unordered-list", "new-line"]
 while True:
-    str_parameter = "plain bold italic header link inline-code ordered-list unordered-list new-line"
-    parameter = ["plain", "bold", "italic",
-                 "header", "link", "inline-code",
-                 "ordered-list", "unordered-list", "new-line"]
+    print("\n".join(markdown))
     choice = input("Choose a formatter: ")
     if choice == "!help":
-        print(f"""Available parameters: {str_parameter}""")
+        print(f"Available formatters: {str_parameter}")
     elif choice == "!done":
         break
+    elif choice == parameter[0]:
+        text = input("Text: ")
+        markdown.append(text)
+    elif choice == parameter[3]:
+        try:
+            level = int(input("Level: "))
+            if 1 <= level <= 6:
+                levels = "#" * level
+                header = input("Text: ")
+                markdown.append(f"{levels} {header}")
+            else:
+                print("Value must be between 1 and 6")
+        except ValueError:
+            print("Enter a numeric value!")
+    elif choice == parameter[8]:
+        markdown.append(" ")
+    elif choice == parameter[4]:
+        label = input("Label: ")
+        url = input("URL: ")
+        markdown.append(f"""[{label}] ({url})""")
     else:
-        print("Unknown formatting type or command")
+        print("Unknown formatter type or command")
