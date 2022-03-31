13ce27cd1c117d9bd1e9cccadc91f45c00d5320b
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-03-31 23:19:10 +0300

    Currency Exchange #3.1 stage

--- Currency_Exchange/currency-exchange.py
+++ Currency_Exchange/currency-exchange.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     while True:
         try:
             currency = input("Enter currency code: ").lower().strip()
+            r = requests.get(f"http://www.floatrates.com/daily/{currency}.json")
+            course = r.json()
+            print(f"{course['usd']['code']} to {currency.upper()} = {round(course['usd']['inverseRate'], 2)}")
+            print(f"{course['eur']['code']} to {currency.upper()} = {round(course['eur']['inverseRate'], 2)}")
             break
         except simplejson.JSONDecodeError:
             print("Incorrect currency code")
-    r = requests.get(f"http://www.floatrates.com/daily/{currency}.json")
-    course = r.json()
-    print(f"{course['usd']['code']} to {currency.upper()} = {round(course['usd']['inverseRate'], 2)}")
-    print(f"{course['eur']['code']} to {currency.upper()} = {round(course['eur']['inverseRate'], 2)}")
+
