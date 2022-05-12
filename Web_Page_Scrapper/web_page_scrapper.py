0e144ba37fa0339bba54fb679f207808150d8952
Savelii <s.i.khizhnikov@student.khai.edu>
2022-05-12 19:43:11 +0300

    Web Page Scrapper stage #2

--- Web_Page_Scrapper/web_page_scrapper.py
+++ Web_Page_Scrapper/web_page_scrapper.py
@@ -1,10 +1,20 @@
+import json
 import requests
+from bs4 import BeautifulSoup
 
 
+# https://www.imdb.com/title/tt0080684/
+# https://www.imdb.com/title/tt0121766/
 if __name__ == "__main__":
     url = input("Input the URL: ")
-    r = requests.get(f"{url}")
+    r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    soup = BeautifulSoup(r.text, "lxml")
     if r.ok:
-        print(f"""\n{r.json()["content"]}""")
+        try:
+            description = json.loads(soup.find("script").text)
+            info = {"title": description["name"], "description": description["description"]}
+            print(info)
+        except json.decoder.JSONDecodeError:
+            print("\nInvalid quote resource!")
     else:
         print("\nInvalid quote resource!")
