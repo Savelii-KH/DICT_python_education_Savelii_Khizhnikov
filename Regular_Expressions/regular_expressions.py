3bb97439a41b96272c8233420fa2375a71dd4cf7
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-05-25 14:40:36 +0300

    Regular_Expressions stage #3

--- Regular_Expressions/regular_expressions.py
+++ Regular_Expressions/regular_expressions.py
@@ -1,6 +1,21 @@
 import re
 
 
-words = ["apple", ".pple", "appl.", ".....", "peach"]
+words = ["apple", "ap", "le", "a", ".", "apwle", "peach"]
+
+
+def stage_3(word) -> str:
+    k = 0
+    for i in word:
+        if re.match(r"[a.]|[p.]|[l.]|[e.]", i):
+            k += 1
+        else:
+            k -= 1
+    if abs(len(word)) == k:
+        return "True"
+    else:
+        return "False"
+
+
 if __name__ == "__main__":
-    [print(f"Input: '{i}|apple'     Output: {bool(re.match(r'[a.][p.][p.][l.][e.]', i))}") for i in words]
+    [print(f"Input: '{i}|apple'     Output: {stage_3(i)}") for i in words]
