5932441ae3c87ea5c06cd827c371aa13819facfb
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-05-25 23:48:32 +0300

    Regular_Expressions stage #6

--- Regular_Expressions/regular_expressions.py
+++ Regular_Expressions/regular_expressions.py
@@ -1,9 +1,13 @@
 import re
 
 
-a = ["colou?r", "colou?r", "colou?r", "colou*r", "colou*r", "colou*r", "col.*r", "col.*r", "col.*r", "col.*r", "col.*$"]
-b = ["color", "colour", "colouur", "color", "colour", "colouur", "color", "colour", "colr", "collar", "colors"]
-
-
 if __name__ == "__main__":
-	[print(f"Input: '{a[i]}|{b[i]}'  Output: {bool(re.match(a[i], b[i]))}") for i in range(len(a))]
+	while True:
+		c = input("Regular • ")
+		d = input("Input • ")
+		if c == "exit":
+			break
+		elif d == "exit":
+			break
+		else:
+			print(f"Input: '{c}|{d}'  Output: {bool(re.findall(c, d))}")
