04836c2faa69086fc42e081ab9654e6d32a5fd11
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-05-25 14:06:26 +0300

    Regular_Expressions stage #2

--- Regular_Expressions/regular_expressions.py
+++ Regular_Expressions/regular_expressions.py
@@ -1,9 +1,6 @@
-s1 = ["a", ".", "", "", "a"]
-s2 = ["a", "a", "a", "", ""]
-result = []
-for i in range(len(s1)):
-    if s2[i] == s1[i] or s1[i] == "." or s1[i] == "":
-        result.append("True")
-    else:
-        result.append("False")
-[print(f"""Input: '{s1[i]}|{s2[i]}'     Output: {result[i]}""") for i in range(len(s1))]
+import re
+
+
+words = ["apple", ".pple", "appl.", ".....", "peach"]
+if __name__ == "__main__":
+    [print(f"Input: '{i}|apple'     Output: {bool(re.match(r'[a.][p.][p.][l.][e.]', i))}") for i in words]
